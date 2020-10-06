5cb6ae38d880d5db39a24d1758e57412cab55e05
GYANNENDRA KUMAR <gyannendra.kumar@samparksolutions.com>
2020-10-06 16:45:42 +0530

    changed

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -3,11 +3,12 @@ package com.test.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- src/main/java/com/test/demo/controller/DataController.java
+++ src/main/java/com/test/demo/controller/DataController.java
@@ -3,6 +3,8 @@ package com.test.demo.controller;
 import com.test.demo.CustomerAlertSubscription;
 import com.test.demo.repository.DataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
