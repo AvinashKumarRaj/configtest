3d5a9ecb2903bb701d8fc49273c84341b81e1a4b
Malaya Sinha <malaya.sinha@samparksolutions.com>
2020-10-09 17:18:13 +0000

    Update DemoApplication.java

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Map;
+import com.vecv.cep.utils.serviceImpl.MetaDataConverterServiceImpl;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	MetaDataConverterServiceImpl metaDataConverterServiceImpl()
+	{
+		return new MetaDataConverterServiceImpl();
 	}
 }
