5c7875b6d1e1a960dce0c76d98167486b7ef5cac
saddamkhan <6894107-saddamkhan@users.noreply.gitlab.com>
2021-02-19 14:33:20 +0530

    added jupitor dependency

--- src/test/java/com/test/demo/SpringMockitoApplicationTests.java
+++ src/test/java/com/test/demo/SpringMockitoApplicationTests.java
@@ -6,7 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
