5d4b4f2f8dbbc31fd7a3427e8e7477f96a0191d2
Avinash Kumar <7133282-avinash.samparksolutions@users.noreply.gitlab.com>
2021-06-03 07:35:48 +0000

    Update DemoController.java

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
 	@GetMapping(value = "/check")
 	public String status() {
-		return "OK";
+		return "My ConfigDemo Project is Working Properly Thanks to Visit";
 	}
 }
